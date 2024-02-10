09574503b52653d37f311c6e6208fd39c07ce050
vithor-silva <flatztr3m@gmail.com>
2024-02-10 10:20:21 -0300

    fix

--- script.js
+++ script.js
@@ -8,18 +8,32 @@ const localidadeSpan = document.querySelector("#localidade");
 const ufSpan = document.querySelector("#uf");
 const ibgeSpan = document.querySelector("#ibge");
 const reloadButton = document.getElementById("reloadButton")
+const states = document.getElementById("states")
+const infoCep = document.getElementById('info-cep')
+const statesContainer = document.getElementById("states-container")
+const imageError = document.getElementById("img-states-error")
+const imageOk = document.getElementById("img-states-ok")
+const resultData = document.getElementById("result-data")
 
 const getValue = () => {
-  const inputValue = inputcep.value;
-  buscaCep(inputValue);
-  result.style.display = "flex";
-  result.style.aligitems = "center";
-  result.style.justiyContent = "center"
-  result.style.flexDirection = "column"
-};
-
-const buscaCep = (inputValue) => {
-  fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
+  let inputValue = inputcep.value
+  if(inputValue.length !== 8) {
+    states.innerHTML = "CEP NÃO ENCONTRADO"
+    states.style.color = "red"
+    infoCep.innerHTML = "Hmm. Não encontramos seu cep!  Utilize apenas números."
+    imageOk.style.display = "none"
+    imageError.style.display = "block"
+    resultData.style.display = "none"
+  }
+    buscaCep(inputValue);
+    result.style.display = "flex";
+    result.style.alignItems = "center";
+    result.style.justifyContent = "center";
+    result.style.flexDirection = "column";
+  }
+  
+  const buscaCep = (inputValue) => {
+    fetch(`https://viacep.com.br/ws/${inputValue}/json/`)
     .then((response) => response.json())
     .then((data) => {
       cepSpan.innerHTML = data.cep;
@@ -29,6 +43,8 @@ const buscaCep = (inputValue) => {
       localidadeSpan.innerHTML = data.localidade;
       ufSpan.innerHTML = data.uf;
       ibgeSpan.innerHTML = data.ibge;
+      imageOk.style.display = "block"
+      imageError.style.display = "none"
     })
     .catch((err) => console.log(err));
 };
@@ -38,5 +54,3 @@ reloadButton.addEventListener("click", reloadPage)
 function reloadPage() {
     location.reload()
 }
-
-
